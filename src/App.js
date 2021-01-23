96bcf2ff621d917ac71e16cf0f7f0e213f7c0d07
Jianing <jianingw321@outlook.com>
2021-01-23 14:47:23 +0800

    front-end

--- src/App.js
+++ src/App.js
@@ -1,7 +1,65 @@
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import { useState } from "react";
+
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "make candle",
+      day: "Jan 25th",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "sheshapes meeting",
+      day: "Jan 23th",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "eat marshala",
+      day: "Jan 24th",
+      reminder: false,
+    },
+  ]);
+
+  // add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 100) + 1; // could cause 2 same id
+
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
+  // delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
-      <h1>Hello from Mica react</h1>
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Task To Show"
+      )}
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Button = ({ color, text, onClick }) => {
+  return (
+    <button
+      className="btn"
+      style={{ backgroundColor: color }}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  color: "rosybrown",
+};
+
+Button.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+export default Button;
